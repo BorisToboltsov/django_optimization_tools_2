303a1f6999a21660a6b2a3dbb59161a4a29cb759
Boris <93197878+BorisToboltsov@users.noreply.github.com>
2022-05-10 10:24:57 +0300

    Lesson 6 (#6)
    * lesson_6. Ready

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -43,7 +43,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
+    # "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
@@ -152,7 +152,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Set login path:
 #   https://docs.djangoproject.com/en/3.2/ref/settings/#login-url
-LOGIN_URL = "authnapp:login"
+LOGIN_URL = "auth:login"
 
 DOMAIN_NAME = "http://test.vcore.su"
 
@@ -200,3 +200,46 @@ SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = secrets_auth["google_client_secret"]
 
 SOCIAL_AUTH_VK_OAUTH2_KEY = secrets_auth["vk_client_id"]
 SOCIAL_AUTH_VK_OAUTH2_SECRET = secrets_auth["vk_client_secret"]
+
+# Django Debug Toolbar --->
+if DEBUG:
+    INSTALLED_APPS.extend([
+        "debug_toolbar",
+        "template_profiler_panel",
+        "django_extensions",
+    ])
+
+
+if DEBUG:
+    MIDDLEWARE.extend([
+        "debug_toolbar.middleware.DebugToolbarMiddleware",
+    ])
+
+# Debgu tool bar settings
+if DEBUG:
+
+    def show_toolbar(request):
+        return True
+
+    DEBUG_TOOLBAR_CONFIG = {
+        "SHOW_TOOLBAR_CALLBACK": show_toolbar,
+    }
+
+    DEBUG_TOOLBAR_PANELS = [
+        # "ddt_request_history.panels.request_history.RequestHistoryPanel",
+        "debug_toolbar.panels.versions.VersionsPanel",
+        "debug_toolbar.panels.timer.TimerPanel",
+        "debug_toolbar.panels.settings.SettingsPanel",
+        "debug_toolbar.panels.headers.HeadersPanel",
+        "debug_toolbar.panels.request.RequestPanel",
+        "debug_toolbar.panels.sql.SQLPanel",
+        "debug_toolbar.panels.templates.TemplatesPanel",
+        "debug_toolbar.panels.staticfiles.StaticFilesPanel",
+        "debug_toolbar.panels.cache.CachePanel",
+        "debug_toolbar.panels.signals.SignalsPanel",
+        "debug_toolbar.panels.logging.LoggingPanel",
+        "debug_toolbar.panels.redirects.RedirectsPanel",
+        "debug_toolbar.panels.profiling.ProfilingPanel",
+        "template_profiler_panel.panels.template.TemplateProfilerPanel",
+    ]
+# <--- Django Debug Toolbar
